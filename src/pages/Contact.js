4833fe02eba859fe1c1a117ee37938fd30e65e2a
Dimitar Micevski <dmicevski@hotmail.com>
2021-01-14 18:00:34 -0500

    removed carousel and added flipping cards to portfolio page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -15,53 +15,30 @@ function Contact() {
       <Container style={{ marginTop: 30 }}>
         <Row>
           <Col size="md-12">
-          <Wrapper>
-          <Form>
-          <div className="=page-header">
-            <h1>Contact Info</h1>
-          </div>
-          <br/>
-          <Form.Group controlId="formBasicName">
-    <Form.Label>Name</Form.Label>
-    <Form.Control type="name" placeholder="Enter name" />
-  </Form.Group>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-  </Form.Group>
+            <Wrapper>
+              <Form>
+                <div className="=page-header">
+                  <h1>Contact Info</h1>
+                </div>
+                <br />
+                <Form.Group controlId="formBasicName">
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control type="name" placeholder="Enter name" />
+                </Form.Group>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
 
-  <Form.Group controlId="ControlTextarea1">
-    <Form.Label>Message</Form.Label>
-    <Form.Control placeholder="Enter message" as="textarea" rows={3} />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
-      {/* <div>
-        <form>
-          <div className="=page-header">
-            <h1>Contact Info</h1>
-          </div>
-          <br/>
-          <div className="form-group">
-            <label for="exampleInputName1">Name</label>
-            <input type="name" class="form-control" id="exampleInputName1" aria-describedby="inputName" placeholder="Enter Name"/>
-          </div>
-          <div className="form-group">
-            <label for="exampleInputEmail1">Email Address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Email"/>
-          </div>
-          <div className="form-group">
-            <label for="exampleFormControlTextarea1">Message</label>
-            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-          </div>
-          <div className="col-auto">
-            <button type="submit" class="btn btn-success mb-2">Submit</button>
-          </div>
-        </form>
-      </div> */}
-    </Wrapper>
+                <Form.Group controlId="ControlTextarea1">
+                  <Form.Label>Message</Form.Label>
+                  <Form.Control placeholder="Enter message" as="textarea" rows={3} />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Form>
+            </Wrapper>
           </Col>
         </Row>
       </Container>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,30 +1,115 @@
 import React from "react";
-import Carousel from "react-bootstrap/Carousel";
+// import Carousel from "react-bootstrap/Carousel";
 import Hero from "../components/Hero";
-import Container from "../components/Container";
+// import Container from "../components/Container";
 import Row from "../components/Row";
-import Col from "../components/Col";
+// import Col from "../components/Col";
 import Img1 from "../assets/DailyPlanner.png";
-import Img2 from "../assets/dash2.png" ;
+import Img2 from "../assets/dash2.png";
 import Img3 from "../assets/Homework_CodeRefractor_index.html.png";
 import Img4 from "../assets/PasswordGenerator2.jpg.png";
 import Img5 from "../assets/StartingPage.png";
 import Img6 from "../assets/StartofGame.png";
+import '../components/FlipCard/style.css';
 
 function Portfolio() {
   return (
     <div>
       <Hero backgroundImage="https://thumbs.dreamstime.com/b/grunge-black-portfolio-word-square-rubber-seal-stamp-white-background-171945189.jpg">
       </Hero>
-      <Container style={{ marginTop: 30 }}>
+      {/* <Container style={{ marginTop: 30 }}> */}
         {/* <Row>
           <Col size="md-12">
             <h1>Welcome To My Portfolio!</h1>
           </Col>
         </Row> */}
         <Row>
-          <Col size="md-12">
-            <Carousel>
+          {/* <Col size="md-12"> */}
+          <div class="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <img src={Img1} alt="Avatar" style={{ width: "400px", height: "400px" }} />
+              </div>
+              <div className="flip-card-back">
+                <h1>John Doe</h1>
+                <p>Architect and Engineer</p>
+                <p>We love that guy</p>
+              </div>
+            </div>
+          </div>
+          
+          <div class="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <img src={Img2} alt="Avatar" style={{ width: "400px", height: "400px" }} />
+              </div>
+              <div className="flip-card-back">
+                <h1>John Doe</h1>
+                <p>Architect and Engineer</p>
+                <p>We love that guy</p>
+              </div>
+            </div>
+          </div>
+          
+          <div class="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <img src={Img3} alt="Avatar" style={{ width: "400px", height: "400px" }} />
+              </div>
+              <div className="flip-card-back">
+                <h1>John Doe</h1>
+                <p>Architect and Engineer</p>
+                <p>We love that guy</p>
+              </div>
+            </div>
+          </div>
+          
+          <div class="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <img src={Img4} alt="Avatar" style={{ width: "400px", height: "400px" }} />
+              </div>
+              <div className="flip-card-back">
+                <h1>John Doe</h1>
+                <p>Architect and Engineer</p>
+                <p>We love that guy</p>
+              </div>
+            </div>
+          </div>
+          <div class="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <img src={Img5} alt="Avatar" style={{ width: "400px", height: "400px" }} />
+              </div>
+              <div className="flip-card-back">
+                <h1>John Doe</h1>
+                <p>Architect and Engineer</p>
+                <p>We love that guy</p>
+              </div>
+            </div>
+          </div>
+          <div class="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <img src={Img6} alt="Avatar" style={{ width: "400px", height: "400px" }} />
+              </div>
+              <div className="flip-card-back">
+                <h1>John Doe</h1>
+                <p>Architect and Engineer</p>
+                <p>We love that guy</p>
+              </div>
+            </div>
+          </div>
+          {/* </Col> */}
+        </Row>
+      {/* </Container> */}
+    </div>
+  );
+}
+
+export default Portfolio;
+
+{/* <Carousel>
               <Carousel.Item>
                 <img
                   className="d-block w-100"
@@ -96,12 +181,4 @@ function Portfolio() {
                   <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                 </Carousel.Caption>
               </Carousel.Item>
-            </Carousel>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
-}
-
-export default Portfolio;
+            </Carousel> */}
