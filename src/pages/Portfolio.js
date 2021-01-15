87ed77d3c6871d2373528465bed4311cc6388e22
Dimitar Micevski <dmicevski@hotmail.com>
2021-01-14 20:20:23 -0500

    imported image tag and applied fluid attribute to it

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -10,6 +10,7 @@ import Img4 from "../assets/PasswordGenerator2.jpg.png";
 import Img5 from "../assets/StartingPage.png";
 import Img6 from "../assets/StartofGame.png";
 import '../components/FlipCard/style.css';
+import Image from "react-bootstrap/Image";
 
 function Portfolio() {
   return (
@@ -20,20 +21,12 @@ function Portfolio() {
           <p className="lead">Below you will see the projects I have worked on with links on the back of each card to their github repo.</p>
         </div>
       </div>
-      {/* <Hero backgroundImage="https://thumbs.dreamstime.com/b/grunge-black-portfolio-word-square-rubber-seal-stamp-white-background-171945189.jpg">
-      </Hero> */}
-      {/* <Container style={{ marginTop: 30 }}> */}
-      {/* <Row>
-          <Col size="md-12">
-            <h1>Welcome To My Portfolio!</h1>
-          </Col>
-        </Row> */}
       <Row>
         {/* <Col size="md-12"> */}
         <div class="flip-card">
           <div className="flip-card-inner">
             <div className="flip-card-front bg-dark text-white">
-              <img src={Img1} alt="Avatar" style={{ width: "400px", height: "400px", marginTop: "40px" }} />
+              <Image src={Img1} alt="Avatar" style={{ width: "400px", height: "400px", marginTop: "40px" }} fluid/>
               <h5 style={{ marginTop: "15px" }}>Work Day Scheduler</h5>
             </div>
             <div className="flip-card-back bg-dark text-white">
@@ -50,7 +43,7 @@ function Portfolio() {
         <div class="flip-card">
           <div className="flip-card-inner">
             <div className="flip-card-front bg-dark text-white">
-              <img src={Img2} alt="Avatar" style={{ width: "400px", height: "400px", marginTop: "40px" }} />
+              <Image src={Img2} alt="Avatar" style={{ width: "400px", height: "400px", marginTop: "40px" }} fluid/>
               <h5 style={{ marginTop: "15px" }}>Weather Dashboard</h5>
             </div>
             <div className="flip-card-back bg-dark text-white">
@@ -58,7 +51,7 @@ function Portfolio() {
               <br />
               <br />
               <h1>Weather Dashboard</h1>
-              <p>Architect and Engineer</p>
+              <p>Type in your city in the search box and get the current weather, a 5-day forecast, the UV index, and windspeed.</p>
             </div>
           </div>
         </div>
@@ -66,7 +59,7 @@ function Portfolio() {
         <div class="flip-card">
           <div className="flip-card-inner">
             <div className="flip-card-front bg-dark text-white">
-              <img src={Img3} alt="Avatar" style={{ width: "400px", height: "400px", marginTop: "40px" }} />
+              <Image src={Img3} alt="Avatar" style={{ width: "400px", height: "400px", marginTop: "40px" }} fluid/>
               <h5 style={{ marginTop: "15px" }}>Code Refactor</h5>
             </div>
             <div className="flip-card-back bg-dark text-white">
@@ -74,7 +67,11 @@ function Portfolio() {
               <br />
               <br />
               <h1>Code Refactor</h1>
-              <p>Architect and Engineer</p>
+              <p>Refactored code in order to improve the accessibility of the given website and optimize it for search engines.</p>
+              <br/>
+              <br/>
+              <br/>
+              <a rel="alternate, noreferrer" target="_blank" href="https://github.com/dspark8916/CodeRefractor">Click here</a>
             </div>
           </div>
         </div>
@@ -82,7 +79,7 @@ function Portfolio() {
         <div class="flip-card">
           <div className="flip-card-inner">
             <div className="flip-card-front bg-dark text-white">
-              <img src={Img4} alt="Avatar" style={{ width: "400px", height: "400px", marginTop: "40px" }} />
+              <Image src={Img4} alt="Avatar" style={{ width: "400px", height: "400px", marginTop: "40px" }} fluid/>
               <h5 style={{ marginTop: "15px" }}>Password Generator</h5>
             </div>
             <div className="flip-card-back bg-dark text-white">
@@ -90,14 +87,14 @@ function Portfolio() {
               <br />
               <br />
               <h1>Password Generator</h1>
-              <p>Architect and Engineer</p>
+              <p>Having trouble figuring out a password?? You're in luck!! With my random password generator you won't have to worry about coming up with a password anymore!! </p>
             </div>
           </div>
         </div>
         <div class="flip-card">
           <div className="flip-card-inner">
             <div className="flip-card-front bg-dark text-white">
-              <img src={Img5} alt="Avatar" style={{ width: "400px", height: "400px", marginTop: "40px" }} />
+              <Image src={Img5} alt="Avatar" style={{ width: "400px", height: "400px", marginTop: "40px" }} fluid/>
               <h5 style={{ marginTop: "15px" }}>Spice It Up!!</h5>
             </div>
             <div className="flip-card-back bg-dark text-white">
@@ -105,14 +102,14 @@ function Portfolio() {
               <br />
               <br />
               <h1>Spice It Up!!</h1>
-              <p>Architect and Engineer</p>
+              <p>Bored?? Having trouble coming up with something to do?? Check out the Spice It Up app where we randomly generate a activity for you to do based on the category you select!!</p>
             </div>
           </div>
         </div>
         <div class="flip-card">
           <div className="flip-card-inner">
             <div className="flip-card-front bg-dark text-white">
-              <img src={Img6} alt="Avatar" style={{ width: "400px", height: "400px", marginTop: "40px" }} />
+              <Image src={Img6} alt="Avatar" style={{ width: "400px", height: "400px", marginTop: "40px" }} fluid/>
               <h5 style={{ marginTop: "15px" }}>Timed Quiz</h5>
             </div>
             <div className="flip-card-back bg-dark text-white">
@@ -120,7 +117,7 @@ function Portfolio() {
               <br />
               <br />
               <h1>Timed Quiz</h1>
-              <p>Architect and Engineer</p>
+              <p>Looking for a quick challenge?? With my timed quiz you get 60 seconds to answer 5 questions and see how you rank on the scoreboard.</p>
             </div>
           </div>
         </div>
